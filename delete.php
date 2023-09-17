ac42edd62e4f5dcefc3ac507723ed9791a3e4126
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-17 14:56:51 +0530

    formatting

--- delete.php
+++ delete.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,20 +8,22 @@
     <link rel="stylesheet" href="style.css">
 
 </head>
+
 <body>
     <?php
     // connection established
-$connection=mysqli_connect("localhost","root","","db");
+    $connection = mysqli_connect("localhost", "root", "", "db");
 
 
-$sql_query="delete from student where id=3";
-$res=mysqli_query($connection,$sql_query);
- echo $res;
-if($res){
+    $sql_query = "delete from student where id=3";
+    $res = mysqli_query($connection, $sql_query);
+    echo $res;
+    if ($res) {
 
-    echo "<h3>Record Successfully Deleted</h3>";
-}
-mysqli_close($connection);
+        echo "<h3>Record Successfully Deleted</h3>";
+    }
+    mysqli_close($connection);
     ?>
 </body>
+
 </html>
--- select.php
+++ select.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,31 +8,33 @@
     <link rel="stylesheet" href="style.css">
 
 </head>
+
 <body>
     <?php
     // connection established
-$connection=mysqli_connect("localhost","root","","db");
+    $connection = mysqli_connect("localhost", "root", "", "db");
 
 
-$sql_query="select * from student";
-$res=mysqli_query($connection,$sql_query);
+    $sql_query = "select * from student";
+    $res = mysqli_query($connection, $sql_query);
 
-// $data=mysqli_fetch_array($res); // return all cells in single single array
-$len=mysqli_num_rows($res); // return rows in tables
+    // $data=mysqli_fetch_array($res); // return all cells in single single array
+    $len = mysqli_num_rows($res); // return rows in tables
 
-// row wise printing
-while($data=mysqli_fetch_array($res)){
-echo $data['id']." ";
-echo $data['rollNo']." ";
-echo $data['name']." ";
-echo $data['Age']." ";
-echo $data['class']." ";
-echo $data['address']." ";
-echo $data['mobile']." ";
-echo $data['email']." ";
-echo "<br>";
-}
-mysqli_close($connection);
+    // row wise printing
+    while ($data = mysqli_fetch_array($res)) {
+        echo $data['id'] . " ";
+        echo $data['rollNo'] . " ";
+        echo $data['name'] . " ";
+        echo $data['Age'] . " ";
+        echo $data['class'] . " ";
+        echo $data['address'] . " ";
+        echo $data['mobile'] . " ";
+        echo $data['email'] . " ";
+        echo "<br>";
+    }
+    mysqli_close($connection);
     ?>
 </body>
+
 </html>
