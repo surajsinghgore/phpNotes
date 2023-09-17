c99f82b2b697b994f0b80f0b1489a71b14983658
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-17 14:46:49 +0530

    select data from database

---
+++ select.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select Data</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <?php
+    // connection established
+$connection=mysqli_connect("localhost","root","","db");
+
+
+$sql_query="select * from student";
+$res=mysqli_query($connection,$sql_query);
+
+// $data=mysqli_fetch_array($res); // return all cells in single single array
+$len=mysqli_num_rows($res); // return rows in tables
+
+// row wise printing
+while($data=mysqli_fetch_array($res)){
+echo $data['id']." ";
+echo $data['rollNo']." ";
+echo $data['name']." ";
+echo $data['Age']." ";
+echo $data['class']." ";
+echo $data['address']." ";
+echo $data['mobile']." ";
+echo $data['email']." ";
+echo "<br>";
+}
+mysqli_close($connection);
+    ?>
+</body>
+</html>
