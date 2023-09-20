d663a2f80c8ac2fc88b80bf53bbc7b34d9e67ab4
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-20 21:01:57 +0530

    formatting in update files

--- update.php
+++ update.php
@@ -1,32 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>update</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
-    
+
+
     <?php
-//establishing connection
-$con=mysqli_connect("localhost","root","","db");
-if($con){
-    $sql_query="update student set name=\"suraj singh\" where id=6";
-    
-$res=mysqli_query($con,$sql_query);
-if($res){
+    //establishing connection
+    $con = mysqli_connect("localhost", "root", "", "db");
+    if ($con) {
+        // update query
+        $sql_query = "update student set name=\"suraj singh\" where id=6";
+        // executing update query
+        $res = mysqli_query($con, $sql_query);
+        // return 1 if true
+        if ($res) {
 
-    echo "<h3>Successfully Updated</h3>";
-}
+            echo "<h3>Successfully Updated</h3>";
+        }
+    }
+    // connection not established successfully
+    else {
 
-}
-
-else{
-
-    echo "<h2>Sorry Something went wrong </h2>";
-}
-?>
+        echo "<h2>Sorry Something went wrong </h2>";
+    }
+    // closing connection
+    mysqli_close($con);
+    ?>
 </body>
+
 </html>
