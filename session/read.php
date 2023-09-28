ec88bfebeb7d37a9853725f5674e6a3f445ae375
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-28 20:54:23 +0530

    session update

--- session/read.php
+++ session/read.php
@@ -2,10 +2,10 @@
 
 session_start();
 if (isset($_SESSION['name'])) {
-        $name = $_SESSION['name'] = "suraj singh";
-        $email = $_SESSION['email'] = "suraj@gmail.com";
+        $name = $_SESSION['name'];
+        $email = $_SESSION['email'];
         echo "<h2>Name " . $name . "</h2>";
-        echo "<h2>Name " . $email . "</h2>";
+        echo "<h2>Email " . $email . "</h2>";
 } else {
     echo "<h1>Session Not created</h1>";
 }
--- session/update.php
+++ session/update.php
@@ -1,3 +1,12 @@
 <?php
 
-?>
+session_start();
+if (isset($_SESSION['name'])) {
+        $newname = $_SESSION['name'] = "test user";
+        $newEmail = $_SESSION['email'] = "test@gmail.com";
+    echo "<h1>Session update successfully</h1>";
+        echo "<h2>New Name " . $newname . "</h2>";
+        echo "<h2>New Email " . $newEmail . "</h2>";
+} else {
+    echo "<h1>Session Not created</h1>";
+}
