421010d44c3a92bb194ba146a20d5004bb662bf2
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-28 20:51:33 +0530

    session read

--- session/read.php
+++ session/read.php
@@ -1,3 +1,11 @@
 <?php
 
-?>
+session_start();
+if (isset($_SESSION['name'])) {
+        $name = $_SESSION['name'] = "suraj singh";
+        $email = $_SESSION['email'] = "suraj@gmail.com";
+        echo "<h2>Name " . $name . "</h2>";
+        echo "<h2>Name " . $email . "</h2>";
+} else {
+    echo "<h1>Session Not created</h1>";
+}
