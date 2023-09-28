b6e3af32811d39fea8140421cf820d7a94193b84
surajsinghgore <surajthakurrs45@gmail.com>
2023-09-28 20:28:08 +0530

    cookie create in php

---
+++ cookies/create.php
@@ -0,0 +1,16 @@
+<?php
+//cookie parameters default values
+// setcookie(
+//     string $name,
+//     string $value = "",
+//     int $expires_or_options = 0,
+//     string $path = "",
+//     string $domain = "",
+//     bool $secure = false,
+//     bool $httponly = false
+// )
+// ! generated cookie for  1 day with secure as parameter so that javascript not access it
+setcookie("message","hello from cookies",time()+86400,"/","localhost",true,true);
+echo "<h1>Cookie Generated Successfully</h1>";
+
+?>
