c3f8e213bb1b2261ed0531ad22661312ff1b47f0
surajsinghgore <surajthakurrs45@gmail.com>
2023-10-30 19:26:39 +0530

    program2

---
+++ PracticalFile/Program2.php
@@ -0,0 +1,12 @@
+<?php
+$num1=20;$num2=30;$num3=40;
+if(($num1>$num2) && ($num1>$num3)){
+    echo "$num1 is greatest among $num2 and $num3";
+}
+if(($num2>$num1) && ($num2>$num3)){
+    echo "$num2 is greatest among $num1 and $num3";
+}
+if(($num3>$num2) && ($num3>$num1)){
+    echo "$num3 is greatest among $num1 and $num2";
+}
+?>
