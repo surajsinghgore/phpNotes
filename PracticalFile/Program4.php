fd719bbc2028209cd99592f226faa6abfaffa968
surajsinghgore <surajthakurrs45@gmail.com>
2023-10-30 19:34:18 +0530

    program4

---
+++ PracticalFile/Program4.php
@@ -0,0 +1,18 @@
+<?php
+$int=123;
+echo "Integer = $int <br>";
+$float=1.459;
+echo "Float = $float <br>";
+$str="Hello World";
+echo "String = $str <br>";
+$bool=true;
+echo "Boolean = $bool <br>";
+$arr=array("Sun","Mon","Tue");
+foreach($arr as $val){
+    echo "$val ";
+}
+echo "<br>";
+$n=null;
+echo "NULL = $n <br>";
+echo "Object";
+?>
