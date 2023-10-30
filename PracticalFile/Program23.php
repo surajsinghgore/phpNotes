64208bce5c92fa09beb81b220db419effd2a2ab3
surajsinghgore <surajthakurrs45@gmail.com>
2023-10-30 20:58:48 +0530

    program23

---
+++ PracticalFile/Program23.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Associative Array</h1>";
+$arr=array("product_id"=>"P01","product_name"=>"pen","product_cost"=>50,"product_qty"=>500);
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+
+?>
