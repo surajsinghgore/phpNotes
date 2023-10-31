2b6849f411fd65d020816accc9607d1af0601945
surajsinghgore <surajthakurrs45@gmail.com>
2023-10-31 17:09:22 +0530

    program27

--- PracticalFile/Program27.php
+++ PracticalFile/Program27.php
@@ -4,4 +4,15 @@ $var2="basket";
 echo "<h1>String Interpolation</h1>";
 echo "I like to play {$var1}ball in summer <br>";
 echo "I like to play {$var2}ball in winter";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<h1>Concatenation Operator (.) </h1>";
+$str1="Welcome";$str2="to";$str3="PHP";
+echo $str1.$str2.$str3;
+
+echo "<h1>Concatenation Assignment Operator</h1>";
+$str="Welcome to PHP";
+echo $str.=" Programming";
 ?>
